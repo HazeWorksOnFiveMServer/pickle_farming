809935a7f9ad94bc562f6f4cf208992e337dfb81
Haze <70538228+HazeWorksOnFiveMServer@users.noreply.github.com>
2023-04-16 04:40:28 +0200

    de translation

---
+++ locales/translations/de.lua
@@ -0,0 +1,17 @@
+Language["de"] = {
+    success_process_item = "Erfolgreich verarbeiteter Gegenstand.",
+    success_exchanged_item = "Erfolgreich ausgetauschte Gegenstände.",
+    doesnt_have_enough_item_to_process = "Du hast keine Gegenstände zur Verarbeitung.",
+    doesnt_have_any_items_exchange = "Du hast keine Gegenstände zum Austausch."
+    failed_process_item = "Fehler beim Verarbeiten des Gegenstands.",
+    process_your_plants = "Drücke ~INPUT_CONTEXT~ um deine Pflanzen zu verarbeiten.",
+    exchange_your_items = "Drücke ~INPUT_CONTEXT~ um deine Gegenstände zu tauschen.",
+    doesnt_have_this_seed = "Du hast diesen Samen nicht.",
+    too_many_plants_active = "Du hast zu viele Pflanzen aktiv.",
+    need_to_have_gardening_pitcher = "Du musst eine Gießkanne haben.",
+    need_to_have_gardening_shovel = "Du brauchst eine Gartenschaufel.",
+    water_the_plant = "Drücke ~INPUT_CONTEXT~ um die Pflanze zu gießen.",
+    harvest_the_plant = "Drücke ~INPUT_CONTEXT~ um die Pflanze zu ernten.",
+    planted_seed = "Das Saatgut wurde gepflanzt.",
+    cant_plant_here =    cant_plant_here = "Du kannst diesen Samen hier nicht pflanzen."
+}
